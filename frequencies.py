cfe9ee020b09ce647943e83fe0d4174d8806b195
Oliver Singer <owsinger@outlook.com>
2022-12-09 00:44:53 +0000

    implemented frequencies

--- frequencies.py
+++ frequencies.py
@@ -2,6 +2,40 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def frequencies(items):
+    canAdd = False
+    count = len(items)
     frequencies = {}
+    newitems = []
+
+    if len(items) == 0:
+        return frequencies
+
+    for i in items:
+        if not (isinstance(i, str)):
+            i = str(i)
+            newitems.append(i)
+        else:
+            newitems.append(i)
+
+    frequencies.update({newitems[0]: 1})
+
+    i = 1
+
+    while count > 1:
+
+        for key in frequencies:
+            if key == newitems[i]:
+                canAdd = True
+                break
+
+        if canAdd:
+            new = frequencies.get(newitems[i])
+            frequencies[newitems[i]] = new+1
+        else:
+            frequencies.update({newitems[i]: 1})
+        i = i+1
+        count = count-1
+        canAdd = False
+
     # Your code goes here
     return frequencies
